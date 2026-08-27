7f965dc2b11bc8df172bd5d71e4f02baf94f3464
pradeepSharmadev <amarahipradeep7319@gmail.com>
2026-08-27 23:31:58 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added main application structure with App component.
    - Integrated Login component for user authentication.
    - Implemented CSS styles for the application using Tailwind CSS and custom styles.
    - Included assets for branding (hero image, React and Vite logos).
    - Configured Vite for React and Tailwind CSS support.
    - Established global CSS variables for theming and styling.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import Login from './components/auth/Login';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Login />
+    </>
+  )
+}
+
+export default App
